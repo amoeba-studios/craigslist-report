2dd99cf81fa7e6300bd8e43a9ebc2d441fe954f9
amoeba-studios <tolin@Tonys-MacBook-Pro-2.local>
2017-12-07 21:53:18 -0800

    - Apartments to search in dictionary list format - More readable brief output of listings - Formatted time string

--- craigs.py
+++ craigs.py
@@ -1,18 +1,54 @@
 import os
 import sys
-import craigslist
+import pprint
 
 from craigslist import CraigslistHousing
 
+queries = [
+    {
+        'name': 'Cedar Point',
+        'site': 'dallas',
+        'category': 'apa',
+        'filters': {
+            'query': '"cedar+point"'
+        }
+    },
+    {
+        'name': 'Thorn Manor',
+        'site': 'dallas',
+        'category': 'apa',
+        'filters': {
+            'query': '"thorn+manor"'
+        }
+    },
+]
 
+
+def time_fmt(datetime):
+    suffix='am'
+    fields = datetime.split(' ')
+    date = fields[0][5:].replace('-', '/')
+    hr = int(fields[1].split(':')[0])
+    if hr >= 12:
+        suffix='pm'
+        if hr > 12:
+            hr -= 12
+    min = fields[1].split(':')[1]
+
+    str = "%s\t%2d:%s%s" % (date, hr, min, suffix)
+    return str
 
 
 if __name__ == '__main__':
 
-    cl = CraigslistHousing(site='dallas', category='apa', filters={'query': '"cedar+point"'})
+    for query in queries:
+        print "\n%s Listings" % query['name']
+        print "------------------------------------------------------------"
+        cl = CraigslistHousing(site=query['site'], category=query['category'], filters=query['filters'])
 
-    cl.show_filters()
+        results = cl.get_results(sort_by='newest', limit=20)
+        for result in results:
+            print("%s:\t(%5s)\t%s" % (time_fmt(result['datetime']), result['price'], result['name']) )
+#            pprint.pprint(result)
+#            print("\n")
 
-    results = cl.get_results(sort_by='newest', limit=20)
-    for result in results:
-        print result
