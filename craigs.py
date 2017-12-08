5f1afed39193460fb37c156bd30a7971ca70df6c
Tony Lin <tolin@platform9.com>
2017-12-08 14:53:23 -0800

    Fixed email notification, html formatted email

--- craigs.py
+++ craigs.py
@@ -1,6 +1,8 @@
 import smtplib
 
 from craigslist import CraigslistHousing
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from datetime import datetime
 
 queries = [
@@ -20,53 +22,83 @@ queries = [
             'query': '"thorn+manor"'
         }
     },
+    {
+        'name': 'Arbor Terrace',
+        'site': 'dallas',
+        'category': 'apa',
+        'filters': {
+            'query': '"arbor+terrace"'
+        }
+    },
 ]
 
 HR = 60 * 60
 DAY = HR * 24
-GREEN_THR = DAY * 2
-YELLOW_THR = DAY * 4
+GREEN_THR = 2
+YELLOW_THR = 4
+GREEN = 'green'
+YELLOW = 'cantaloupe'
+RED = 'cranberry'
+
+FROM = ''
+TO = ['']
+PASSWORD = ''
 
 
 def time_fmt(date_time):
 
     post_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M')
     delta = (datetime.now() - post_time).total_seconds()
-    if delta < GREEN_THR:
-        color = 'green'
-    elif delta < YELLOW_THR:
-        color = 'yellow'
+    if delta < GREEN_THR * DAY:
+        color = GREEN
+    elif delta < YELLOW_THR * DAY:
+        color = YELLOW
     else:
-        color = 'red'
+        color = RED
 
     t_str = post_time.strftime('%m/%d %I:%M%p')
-    ret = color + ' ' + t_str
+    ret = '<font color = %s>' % color + t_str + '</font>'
     return ret
 
 
-if __name__ == '__main__':
-
-    # for query in queries:
-    #     print "\n%s Listings" % query['name']
-    #     print "------------------------------------------------------------"
-    #     cl = CraigslistHousing(site=query['site'], category=query['category'], filters=query['filters'])
-    #
-    #     results = cl.get_results(sort_by='newest', limit=20)
-    #     for result in results:
-    #         print("%s:\t(%5s)\t%s" % (time_fmt(result['datetime']), result['price'], result['name']) )
-    fromA = ''
-    to = ''
-    subj = 'Test Subject'
-    date = '12/8/2017'
-    txt = 'Testing Body'
-
-    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (fromA, to, subj, date, txt)
+def mailit(subject, body, to, _from):
     try:
         server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
         server.starttls()
-        server.login('', '')
-        server.sendmail(fromA, to, msg)
-        print "sendmail succeeded"
+        server.login(_from, PASSWORD)
+        print "Email sent successful"
     except Exception as e:
-        print "sendmail failed"
-        print e
+        print "Email send failed. %s" % e
+        return
+
+    message = MIMEMultipart('alternative')
+    message['Subject'] = subject
+    message['To'] = ', '.join(to)
+    message['From'] = _from
+    message.attach(MIMEText(body, 'html'))
+
+    server.sendmail(_from, to, message.as_string())
+    server.quit()
+
+
+if __name__ == '__main__':
+    body = ""
+    for query in queries:
+        body += "%s Listings<BR>" % query['name']
+        body += "----------------------------------<BR>"
+        cl = CraigslistHousing(site=query['site'], category=query['category'], filters=query['filters'])
+
+        results = cl.get_results(sort_by='newest', limit=20)
+        for result in results:
+            body += ("%s:\t(%5s)\t<a href=%s>%s</a>\n<BR>" %
+                     (time_fmt(result['datetime']), result['price'], result['url'], result['name']))
+        body += '<BR><BR>'
+
+    body += "<font color = %s>Green</font> - Under %d days old<BR>" % (GREEN, GREEN_THR)
+    body += "<font color = %s>Yellow</font> - Over %d days old<BR>" % (YELLOW, GREEN_THR)
+    body += "<font color = %s>Red</font> - Over %d days old<BR>" % (RED, YELLOW_THR)
+
+    subj = 'Daily apartment Listings'
+
+    mailit(subj, body, TO, FROM)
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='craigslist',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='tolin',
+    author_email='',
+    description=''
+)
